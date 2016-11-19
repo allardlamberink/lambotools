7901156f4674b138dbf7f7e38ed5403f13ae3536
root <root@ns0.mcseo.nl>
2016-11-19 22:54:10 +0100

    commit working version

--- update_soa.py
+++ update_soa.py
@@ -3,7 +3,7 @@
 import MySQLdb
 from datetime import date
 import sys
-import settings
+from settings import *
 
 default_prio = 14400
 
@@ -15,25 +15,30 @@ def get_soa_for_domain(domainname):
         if crs.rowcount > 0:
                 row = crs.fetchone()
                 domain_id = row["id"]
-                print "domain {0} gevonden in db, domein id={1}".format(domainname, domain_id)
+                print "domain {0} gevonden in db, domain id={1}".format(domainname, domain_id)
 		print "trying to get SOA-record"
         	crs.execute("""SELECT content FROM records WHERE domain_id = %s and type ='SOA'""", (domain_id,))
         	if crs.rowcount > 0:
                 	row = crs.fetchone()
 			print "SOA for domain {0} = {1}".format(domainname, row["content"])
+                        old_soa = row['content'].split()
+                        # generate new SOA serial:
+                        # if soa date is today
+                        if old_soa[2][:8] == date.today().strftime("%Y%m%d"):
+                                old_soa[2] = str(int(old_soa[2]) + 1)
+				# todo: what todo if SOA-serial > 99?
+                        else:
+                                old_soa[2] = date.today().strftime("%Y%m%d") + '00'
+                        new_soa = ' '.join(old_soa)
+                        # updaten SOA
+                        crs.execute("""UPDATE records SET content = %s WHERE domain_id = %s AND type ='SOA'""", (new_soa,domain_id,))
+                        print "succesfully updated domain: {0}".format(domainname)
+			db.commit()
 		else:
 			print "no SOA record found for domain {0}".format(domainname)
 	else:
 		print "the given domain is not found in db"
 
-	#new_soa = ' '.join(old_soa)
-	# updaten SOA
-	#crs.execute("""UPDATE records SET content = %s WHERE domain_id = %s AND type ='SOA'""", (new_soa,domain_id,))
-	#print "succesfully updated domein: {0}".format(domainname)
-	#print "nr of sql_actions voor domein {0} = {1}".format(domainname, sql_actions)
-
-        print "\n"
-	#        db.commit()
         db.close()
 
 # let op in argv[0] zit altijd de naam van het script zelf
